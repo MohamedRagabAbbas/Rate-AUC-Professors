53a107014f734c3e64d8319ed0f615d029899e8d
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-04-06 13:07:01 +0200

    add more attributes and relations to department model

--- rate-auc-backend/Models/Department.cs
+++ rate-auc-backend/Models/Department.cs
@@ -1,10 +1,14 @@
-﻿namespace RateAucProfessors.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RateAucProfessors.Models
 {
     public class Department
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
 
+        public ICollection<Professor>? Professors { get; set; }
     }
 }
