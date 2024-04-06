1685195cbe97c7ce966714f75b104c4c39cbe270
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-04-06 13:24:48 +0200

    add attributes and relations to comment and reply moldes

--- rate-auc-backend/Models/Reply.cs
+++ rate-auc-backend/Models/Reply.cs
@@ -1,9 +1,20 @@
-﻿namespace RateAucProfessors.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RateAucProfessors.Models
 {
     public class Reply
     {
         public int Id { get; set; }
+
+        [Required]
         public string Content { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+
+
+        // Relationships
+        public int CommentId { get; set; }
+        public Comment? Comment { get; set; }
         public int StudentId { get; set; }
+        public Student? Student { get; set; }
     }
 }
