f9411c9016d24ca6bd1967af057ae14fed2f2975
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-04-07 00:44:36 +0200

    add general apis for course, student, professor, department, lecture, and note

---
+++ rate-auc-backend/Controllers/CourseController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RateAucProfessors.IRepository;
+using RateAucProfessors.Models;
+
+namespace RateAucProfessors.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+                public CourseController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        [Route("get-all")]
+        public async Task<IActionResult> GetAll()
+        {
+            var courses = await _unitOfWork.Course.GetAllAsync();
+            return Ok(courses);
+        }
+        [HttpGet]
+        [Route("get-by-id/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var course = await _unitOfWork.Course.GetByIdAsync(id);
+            return Ok(course);
+        }
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> Add(Course course)
+        {
+            var result = await _unitOfWork.Course.Add(course);
+            return Ok(result);
+        }
+        [HttpPut]
+        [Route("update")]
+        public IActionResult Update(Course course)
+        {
+            var result = _unitOfWork.Course.Update(course);
+            return Ok(result);
+        }
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _unitOfWork.Course.Delete(id);
+            return Ok(result);
+        }
+    }
+}
---
+++ rate-auc-backend/Controllers/SyllabusController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RateAucProfessors.IRepository;
+using RateAucProfessors.Models;
+
+namespace RateAucProfessors.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SyllabusController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public SyllabusController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        [Route("get-all")]
+        public async Task<IActionResult> GetAll()
+        {
+            var syllabuses = await _unitOfWork.Syllabus.GetAllAsync();
+            return Ok(syllabuses);
+        }
+        [HttpGet]
+        [Route("get-by-id/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var syllabus = await _unitOfWork.Syllabus.GetByIdAsync(id);
+            return Ok(syllabus);
+        }
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> Add(Syllabus syllabus)
+        {
+            var result = await _unitOfWork.Syllabus.Add(syllabus);
+            return Ok(result);
+        }
+        [HttpPut]
+        [Route("update")]
+        public IActionResult Update(Syllabus syllabus)
+        {
+            var result = _unitOfWork.Syllabus.Update(syllabus);
+            return Ok(result);
+        }
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _unitOfWork.Syllabus.Delete(id);
+            return Ok(result);
+        }
+    }
+}
