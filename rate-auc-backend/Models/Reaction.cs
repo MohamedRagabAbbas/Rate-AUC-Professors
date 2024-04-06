7156bfedfe062e2575c9881085b521d8a36fa7b6
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-04-06 13:27:15 +0200

    add attributes and relations to Reaction model

--- rate-auc-backend/Models/Reaction.cs
+++ rate-auc-backend/Models/Reaction.cs
@@ -1,8 +1,20 @@
-﻿namespace RateAucProfessors.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RateAucProfessors.Models
 {
     public class Reaction
     {
         public int Id { get; set; }
+
+        [Required]
+        public bool IsLike { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+
+        // Relationships
+        public int FeedId { get; set; }
+        public Feed? Feed { get; set; }
+
         public int StudentId { get; set; }
+        public Student? Student { get; set; }
     }
 }
