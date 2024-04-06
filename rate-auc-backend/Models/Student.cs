ce0df40dd82dd9058762b5040a0354dc56fe9159
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-04-06 12:48:18 +0200

    add more attributes to student

--- rate-auc-backend/Models/Student.cs
+++ rate-auc-backend/Models/Student.cs
@@ -4,6 +4,10 @@ namespace RateAucProfessors.Models
 {
     public class Student : IdentityUser
     {
-        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Major { get; set; } = string.Empty;
+        public string Standing { get; set; } = string.Empty;
+        public string GraduationYear { get; set; } = string.Empty;
     }
 }
