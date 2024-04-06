9ee424cc91be2ccd424fba6f9606e2c4b7563a32
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-04-06 13:15:27 +0200

    add attributes and relations to assignment model

--- rate-auc-backend/Models/Assignment.cs
+++ rate-auc-backend/Models/Assignment.cs
@@ -2,5 +2,16 @@
 {
     public class Assignment
     {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime UploadDate { get; set; } = DateTime.Now;
+
+        //relationships
+        public int StudentId { get; set; }
+        public Student? Student { get; set; }
+        public int ProfessorId { get; set; }
+        public Professor? Professor { get; set; }
+        public int CourseId { get; set; }
+        public Course? Course { get; set; }
     }
 }
