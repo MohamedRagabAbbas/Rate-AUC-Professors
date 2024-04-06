18721f78da18e5e56cbed9130f0d21b4ff8680e4
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-04-06 13:12:17 +0200

    add attributes and relations to syllabus, note, and lecture

--- rate-auc-backend/Models/Syllabus.cs
+++ rate-auc-backend/Models/Syllabus.cs
@@ -1,6 +1,20 @@
-﻿namespace RateAucProfessors.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RateAucProfessors.Models
 {
     public class Syllabus
     {
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        public DateTime UploadDate { get; set; } = DateTime.Now;
+
+        //Relationships
+        public int StudentId { get; set; }
+        public Student? Student { get; set; } 
+        public int ProfessorId { get; set; }
+        public Professor? Professor { get; set; }
+        public int CourseId { get; set; }
+        public Course? Course { get; set; }
     }
 }
