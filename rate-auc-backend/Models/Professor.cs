a7e2afb38507c750802d16fbb58df32509519d7b
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-04-06 13:01:22 +0200

    add professor relationships and modify student model

--- rate-auc-backend/Models/Professor.cs
+++ rate-auc-backend/Models/Professor.cs
@@ -1,9 +1,22 @@
-﻿namespace RateAucProfessors.Models
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RateAucProfessors.Models
 {
     public class Professor
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string Email { get; set; } = string.Empty;
+        public string Bio { get; set; } = string.Empty;
+
+        // Relationships
+
+        public int DepartmentId { get; set; }
+        public Department? Department { get; set; }
+        public ICollection<Course>? Courses { get; set; }
+        public ICollection<Rating>? Ratings { get; set; }
     }
 }
