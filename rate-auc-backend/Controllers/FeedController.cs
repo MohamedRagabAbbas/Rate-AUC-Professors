60591751dde0f831a5e83e48e5eafc7fb357665e
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-04-07 00:48:31 +0200

    add general apis for feeds

--- rate-auc-backend/Controllers/FeedController.cs
+++ rate-auc-backend/Controllers/FeedController.cs
@@ -7,10 +7,10 @@ namespace RateAucProfessors.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AssignmentController : ControllerBase
+    public class FeedController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
-        public AssignmentController(IUnitOfWork unitOfWork)
+        public FeedController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
@@ -18,36 +18,37 @@ namespace RateAucProfessors.Controllers
         [Route("get-all")]
         public async Task<IActionResult> GetAll()
         {
-            var assignments = await _unitOfWork.Assignment.GetAllAsync();
-            return Ok(assignments);
+            var feeds = await _unitOfWork.Feed.GetAllAsync();
+            return Ok(feeds);
         }
         [HttpGet]
         [Route("get-by-id/{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            var assignment = await _unitOfWork.Assignment.GetByIdAsync(id);
-            return Ok(assignment);
+            var feed = await _unitOfWork.Feed.GetByIdAsync(id);
+            return Ok(feed);
         }
         [HttpPost]
         [Route("add")]
-        public async Task<IActionResult> Add(Assignment assignment)
+        public async Task<IActionResult> Add(Feed feed)
         {
-            var result = await _unitOfWork.Assignment.Add(assignment);
+            var result = await _unitOfWork.Feed.Add(feed);
             return Ok(result);
         }
         [HttpPut]
         [Route("update")]
-        public IActionResult Update(Assignment assignment)
+        public IActionResult Update(Feed feed)
         {
-            var result = _unitOfWork.Assignment.Update(assignment);
+            var result = _unitOfWork.Feed.Update(feed);
             return Ok(result);
         }
         [HttpDelete]
         [Route("delete/{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            var result = await _unitOfWork.Assignment.Delete(id);
+            var result = await _unitOfWork.Feed.Delete(id);
             return Ok(result);
         }
+
     }
 }
--- rate-auc-backend/obj/Debug/net7.0/RateAucProfessors.AssemblyInfo.cs
+++ rate-auc-backend/obj/Debug/net7.0/RateAucProfessors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9411c9016d24ca6bd1967af057ae14fed2f2975")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76630389cb980a3250a7d11441db9f844c6580d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
