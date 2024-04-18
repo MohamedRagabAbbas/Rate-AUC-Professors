48e7af8a68a7bbf16a68b91b81f1a14f7a8290ad
mahmoudnour2 <mahmoudnour@aucegypt.edu>
2024-04-18 12:32:51 +0200

    removed professor ID

--- rate-auc-backend/Models/Professor.cs
+++ rate-auc-backend/Models/Professor.cs
@@ -5,14 +5,12 @@ namespace RateAucProfessors.Models
 {
     public class Professor
     {
-        public int Id { get; set; }
-        [Required]
         public string Name { get; set; } = string.Empty;
         [Required]
         public string Email { get; set; } = string.Empty;
         public string Bio { get; set; } = string.Empty;
         public bool gender { get; set; } = false;
-
+        
         // Relationships
         public int DepartmentId { get; set; }
         public Department? Department { get; set; }
