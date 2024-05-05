fe4fb2bb8b8f8a84380ebc66dfc5f213895405df
mahmoudnour2 <mahmoudnour@aucegypt.edu>
2024-05-05 03:50:52 +0300

    updated_on_delete_and_pushed_courses_data

--- rate-auc-backend/obj/Release/net7.0/RateAucProfessors.AssemblyInfo.cs
+++ rate-auc-backend/obj/Release/net7.0/RateAucProfessors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e6d92bf9061a3a7911eb0b98e9c7a89989a986b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867570cc0b8e654e13a4a511c65e35a8861e2704")]
 [assembly: System.Reflection.AssemblyProductAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
