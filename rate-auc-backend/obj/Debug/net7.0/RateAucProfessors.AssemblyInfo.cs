cfdee337ab8ab866545925df5b49b55ea1f91685
mahmoudnour2 <mahmoudnour@aucegypt.edu>
2024-05-09 02:47:38 +0300

    update_courses_table

--- rate-auc-backend/obj/Debug/net7.0/RateAucProfessors.AssemblyInfo.cs
+++ rate-auc-backend/obj/Debug/net7.0/RateAucProfessors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d3817596c5b16610960fb071e4eb2a144e8ca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7c6106f635cc9a15af3cffd8f5351c0170df62")]
 [assembly: System.Reflection.AssemblyProductAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
