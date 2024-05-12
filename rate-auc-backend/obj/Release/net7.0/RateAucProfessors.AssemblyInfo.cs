a032cd8fa0a589c505d3fe9dbd7bcd095bacc832
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-05-12 14:42:03 +0300

    tried to solved the conflict

--- rate-auc-backend/obj/Release/net7.0/RateAucProfessors.AssemblyInfo.cs
+++ rate-auc-backend/obj/Release/net7.0/RateAucProfessors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82bf351a39412923bd1fefd66bc6a2a079cc9c4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7c6106f635cc9a15af3cffd8f5351c0170df62")]
 [assembly: System.Reflection.AssemblyProductAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
