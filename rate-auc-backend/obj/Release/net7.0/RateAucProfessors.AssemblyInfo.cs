f25f798ddf182a46af104d881bc847be1ecfbbb1
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-05-19 23:52:31 +0300

    added configuration files to remove merging conflict

--- rate-auc-backend/obj/Release/net7.0/RateAucProfessors.AssemblyInfo.cs
+++ rate-auc-backend/obj/Release/net7.0/RateAucProfessors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7c6106f635cc9a15af3cffd8f5351c0170df62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a691f6a2483a1bdccb74841fa4e8bc30e4563f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RateAucProfessors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
