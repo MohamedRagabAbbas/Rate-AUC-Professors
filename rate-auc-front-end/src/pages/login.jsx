ac5cd8f9c8d25eb562200a8b47a93c78afda06e0
zeinfnoureddin <zein.ne.30@gmail.com>
2024-05-20 10:57:55 +0300

    working on authentication

--- rate-auc-front-end/src/pages/login.jsx
+++ rate-auc-front-end/src/pages/login.jsx
@@ -1,41 +1,44 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './login_box.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./login_box.css";
 
-import axios from 'axios';
+import axios from "axios";
 const Login = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-   
-  
+
     const userData = {
       email,
-      password
+      password,
     };
-  
+
     try {
-      const response = await axios.post("http://localhost:5243/api/Authentication/authenticate", userData, {
-        headers: {
-          "Content-Type": "application/json"
+      const response = await axios.post(
+        "http://localhost:5243/api/Authentication/authenticate",
+        userData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      });
-  
+      );
+
       if (response.data.isAuthenticated) {
-        console.log('Login success:', response.data);
-        navigate('/feed'); 
+        console.log("Login success:", response.data);
+        localStorage.setItem("authToken", response.data.token);
+        navigate("/feed");
       } else {
-        alert('You are not a recognized user. Please sign up.');
+        alert("You are not a recognized user. Please sign up.");
       }
     } catch (error) {
-      console.error('Login failed:', error.message);
-      alert('Login failed: ' + error.message);
+      console.error("Login failed:", error.message);
+      alert("Login failed: " + error.message);
     }
   };
-
 
   const handleSignUp = () => {
     navigate("/sign_up");
@@ -67,10 +70,12 @@ const Login = () => {
           <input type="checkbox" id="rememberMe" />
           <label htmlFor="rememberMe">Remember me</label>
         </div>
-        <button type="submit" className="login-button">Login</button>
+        <button type="submit" className="login-button">
+          Login
+        </button>
         <button type="button" onClick={handleSignUp} className="sign-up-button">
           Sign Up
-        </button> 
+        </button>
       </form>
     </div>
   );
